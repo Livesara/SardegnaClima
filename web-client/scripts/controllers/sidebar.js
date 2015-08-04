3eef260cd48690d602a5f67855e3142c9d058464
Sara  Balbo <sarabalbo@libero.it>
2015-08-04 23:52:42 +0200

    Change of the sidebar
    Cambiamento della barra laterale e inserimento di tre nuove voci: Dati
    attuali, Previsioni Meteo, Informazioni locali

---
+++ web-client/scripts/controllers/sidebar.js
@@ -0,0 +1,16 @@
+use strict';
+
+/**
+ * @ngdoc function
+ * @name sardegnaclima.controller:MainCtrl
+ * @description
+ * # MainCtrl
+
+ */
+
+
+
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('CollapseDatiAttuali', function ($scope) {
+  $scope.isCollapsed = false;
+});
